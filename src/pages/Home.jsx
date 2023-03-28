78f960c4d4d237f5fd71f30926d3e6566eb2bc6d
Vlas-pravsha <vlas20421@gmail.com>
2023-03-28 21:55:56 +0300

    message

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,13 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { useState, useEffect } from "react";
+import Pagination from "../components/Pagination";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setcurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: "популярности",
     sortProperty: "rating",
@@ -19,8 +21,9 @@ const Home = () => {
     const sortBy = sortType.sortProperty.replace("-", "");
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
     fetch(
-      `https://6413ad9ec469cff60d684431.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6413ad9ec469cff60d684431.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -28,12 +31,13 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   const skeletons = [...new Array(6)].map((_, index) => (
     <Skeleton key={index} />
   ));
-  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   return (
     <div className="  container">
@@ -44,8 +48,9 @@ const Home = () => {
         />
         <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
-      <h2 className="content__title">Все пиццьлы</h2>
+      <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setcurrentPage(number)} />
     </div>
   );
 };
